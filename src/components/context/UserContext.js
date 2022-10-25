a909f8ce1888cd18be543391409cb4a9e9696ce0
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-10-25 21:56:36 +0600

    add-authentication-system-with-popup

--- src/components/context/UserContext.js
+++ src/components/context/UserContext.js
@@ -1,16 +1,61 @@
 import React from "react";
 import { createContext } from "react";
 import app from "../../firebase/firebase.config";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
 const UserContext = ({ children }) => {
   const [user, setUser] = useState({});
 
-  const authInfo = { user };
-
+  //Create a new account by passing the new user's email address and password to createUserWithEmailAndPassword:
+  const registerUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+  //When a user signs in to your app, pass the user's email address and password to signInWithEmailAndPassword:
+  const loggedInUser = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  };
+  //The recommended way to get the current user is by setting an observer on the Auth object:
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+  //To sign in with a pop-up window, call signInWithPopup with google:
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider);
+  };
+  //To sign in with a pop-up window, call signInWithPopup with github:
+  const signInWithGitHub = () => {
+    signInWithPopup(auth, gitHubProvider);
+  };
+  //to sign out a user, call signOut:
+  const signOutUser = () => {
+    signOut(auth);
+  };
+  const authInfo = {
+    user,
+    registerUser,
+    loggedInUser,
+    signOutUser,
+    signInWithGoogle,
+    signInWithGitHub,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
