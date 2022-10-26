752c3c9d8583e471c88699373e095d21a58a258f
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-10-26 19:14:58 +0600

    update-download-pdf-button

--- src/components/courseDetails/CourseDetails.js
+++ src/components/courseDetails/CourseDetails.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import Pdf from "react-to-pdf";
 
+const ref = React.createRef();
 const CourseDetails = () => {
   const courseDetail = useLoaderData();
   const { id, courseHeader, courseThumb, courseDes } = courseDetail;
@@ -13,11 +15,23 @@ const CourseDetails = () => {
         <div class="col-md-8">
           <div class="card-body">
             <h2 class="card-title">{courseHeader}</h2>
-            <p class="card-text">{courseDes}</p>
-            <button className="btn btn-danger">Download Pdf</button>
+            <p ref={ref} class="card-text">
+              {courseDes}
+            </p>
+            <Pdf targetRef={ref} filename="course-info.pdf">
+              {({ toPdf }) => (
+                <button
+                  onClick={toPdf}
+                  className="btn btn-danger d-lg-inline d-md-inline d-block"
+                >
+                  Download Pdf
+                </button>
+              )}
+            </Pdf>
+
             <Link
               to={`/course/detail/${id}`}
-              className="btn btn-dark ms-lg-3 ms-md-3 ms-0 mt-lg-0 mt-md-0 mt-3"
+              className="btn btn-dark ms-lg-3 ms-md-3 ms-0 mt-lg-0 mt-md-0 mt-3 "
             >
               Get Premium Access
             </Link>
